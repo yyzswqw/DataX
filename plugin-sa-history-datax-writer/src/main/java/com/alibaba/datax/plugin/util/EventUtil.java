47d0a04073d0fe3cbb1d5b8aa327fa11d18cbaaf
wangqiwei <wangqiwei@sensorsdata.cn>
2021-12-15 16:18:22 +0800

    sahistorywriter写插件修改idm3插入逻辑

--- plugin-sa-history-datax-writer/src/main/java/com/alibaba/datax/plugin/util/EventUtil.java
+++ plugin-sa-history-datax-writer/src/main/java/com/alibaba/datax/plugin/util/EventUtil.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 public class EventUtil {
@@ -25,6 +26,7 @@ public class EventUtil {
             properties.remove(KeyConstant.EVENT_IS_LOGIN_ID);
             properties.remove(KeyConstant.EVENT_EVENT_NAME);
             sa.track(distinctId, eventIsLoginId, eventEventName, properties);
+            sa.flush();
         } catch (Exception e) {
             log.error("Event Exception: {}", e);
             e.printStackTrace();
@@ -34,23 +36,37 @@ public class EventUtil {
     public static void process(SensorsAnalytics sa, Map<String, Object> properties, List<IdentityItem> identityList) {
         String eventEventName = (String) properties.get(KeyConstant.EVENT_EVENT_NAME);
         SensorsAnalyticsIdentity.Builder builder = SensorsAnalyticsIdentity.builder();
+        int count = 0;
         for (IdentityItem identityItem : identityList) {
             if(identityItem.isColumn()){
                 Object value = properties.getOrDefault(identityItem.getColumn(), null);
                 if(!NullUtil.isNullOrBlank(value)){
                     builder.addIdentityProperty(identityItem.getIdName(),value.toString());
+                }else{
+                    //记录空值数
+                    count++;
                 }
             }else{
-                builder.addIdentityProperty(identityItem.getIdName(),identityItem.getColumn());
+                if(!NullUtil.isNullOrBlank(identityItem.getColumn())){
+                    builder.addIdentityProperty(identityItem.getIdName(),identityItem.getColumn());
+                }else{
+                    //记录空值数
+                    count++;
+                }
             }
         }
         identityList.forEach(i->properties.remove(i.getColumn()));
         SensorsAnalyticsIdentity identity = builder.build();
+        Map<String, String> identityMap = identity.getIdentityMap();
+        if(Objects.isNull(identityMap) || identityMap.isEmpty() || identityList.size() == count){
+            return;
+        }
         try {
             properties.remove(KeyConstant.EVENT_DISTINCT_ID_COL);
             properties.remove(KeyConstant.EVENT_IS_LOGIN_ID);
             properties.remove(KeyConstant.EVENT_EVENT_NAME);
             sa.trackById(identity,eventEventName, properties);
+            sa.flush();
         } catch (Exception e) {
             log.error("Event Exception: {}", e);
             e.printStackTrace();
